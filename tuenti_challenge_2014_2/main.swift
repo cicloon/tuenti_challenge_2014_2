fb1b388c7461430d0d300e17f19f27721e12b68a
cicloon <aleon.prof@gmail.com>
2015-03-13 23:14:56 +0100

    Initial Commit

---
+++ tuenti_challenge_2014_2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  tuenti_challenge_2014_2
+//
+//  Created by cicloon on 13/3/15.
+//  Copyright (c) 2015 cicloon. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
